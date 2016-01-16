7c05081ed671a32504290ddb5b7a8af5dcbc15bf
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-01-16 17:19:31 -0500

    working using compiled on different node versions

---
+++ dist/compiled.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var lodash_fp = require('lodash/fp');
+
+const add = (a, b) => a + b;
+
+const sum = add(2, 3);
+console.log(`2 + 3 = ${ sum }`);
+
+const a = ['foo', 'bar', 'baz'];
+console.log(`first element of ${ a } is`, lodash_fp.head(a));
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import { add } from './calc'
+import { head } from 'lodash/fp'
+
+const sum = add(2, 3)
+console.log(`2 + 3 = ${sum}`)
+
+const a = ['foo', 'bar', 'baz']
+console.log(`first element of ${a} is`, head(a))
+
